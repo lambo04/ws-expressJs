2358b3b92a8a914e5205e8063aeca8bee2d61c91
lambo04 <omarmidouni0@gmail.com>
2024-12-02 09:59:48 +0100

    fini

--- middleware/verify.js
+++ middleware/verify.js
@@ -1,11 +1,20 @@
+const fs = require("fs");
+
 module.exports = function(req, res, next) {
     const currentDate = new Date();
     const currentDay = currentDate.getDay();  // 0 = Dimanche, 1 = Lundi, ..., 6 = Samedi
     const currentHour = currentDate.getHours(); // Heure actuelle
 
-    if (currentDay >= 0 && currentDay <= 5 && currentHour >= 0 && currentHour <= 17) {
+    if (currentDay >= 1 && currentDay <= 5 && currentHour >= 0 && currentHour <= 17) {
         return next();  // Accès autorisé pendant les heures de travail
-    } else {
-        res.sendFile(__dirname + '/../public/views/error.html'); // Page d'erreur si hors des heures
-    }
-};
+    }else {
+            fs.readFile("./public/views/error.html", (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.send("Erreur interne du serveur");
+                } else {
+                    res.end(data);
+                }
+            });
+        }
+    };
